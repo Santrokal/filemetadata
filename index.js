6aa123f226fb552ee284ab04595b1c73e24a77ad
SantroKal <msalman90826@gmail.com>
2025-06-20 10:58:14 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+
+const storage = multer.diskStorage({})
+const upload = multer({storage: storage})
 
 var app = express();
 
+
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).json({ error: 'File not selected' });
+  }
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
